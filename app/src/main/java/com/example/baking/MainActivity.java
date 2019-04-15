9e060e414a7d83d3fb95b58138394c3de6d92604
Archangel354 <rduncan354@gmail.com>
2019-04-14 21:58:04 -0400

    Added mRecyclerView definition and findViewByID

--- app/src/main/java/com/example/baking/MainActivity.java
+++ app/src/main/java/com/example/baking/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.baking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView =  findViewById(R.id.recycler_view);
     }
 }
