b101e5586d7946b867932747d428da88e595bf03
Archangel354 <rduncan354@gmail.com>
2019-04-30 11:54:13 -0400

    Junk commit, added log files to Adapter.java

--- app/src/main/java/com/example/baking/RecipeAdapter.java
+++ app/src/main/java/com/example/baking/RecipeAdapter.java
@@ -3,6 +3,7 @@ package com.example.baking;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -44,6 +45,9 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeView
         String recipeName = currentRecipe.getName();
         String recipeServings = currentRecipe.getServings();
         String recipeImage = currentRecipe.getImage();
+        Log.i("LOG RecipeAdapter", "recipeName: " + recipeName);
+        Log.i("LOG RecipeAdapter", "recipeServings: " + recipeServings);
+        Log.i("LOG RecipeAdapter", "recipeImage: " + recipeImage);
 
         holder.rName.setText(recipeName);
         holder.rServings.setText(recipeServings);
