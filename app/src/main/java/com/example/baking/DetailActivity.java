926f2b937d04c8988be8268f3371c7fd5e1190c2
Archangel354 <rduncan354@gmail.com>
2019-05-14 08:12:13 -0400

    mapped recyclerDetail_view with findbyID in DetailActivity

--- app/src/main/java/com/example/baking/DetailActivity.java
+++ app/src/main/java/com/example/baking/DetailActivity.java
@@ -2,12 +2,18 @@ package com.example.baking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
 
 public class DetailActivity extends AppCompatActivity {
+
+    private RecyclerView dRecyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        dRecyclerView = findViewById(R.id.recyclerDetail_view);
     }
 }
