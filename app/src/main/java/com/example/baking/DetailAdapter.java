90f3da6cf24d1191e9b38c1564faeee8ed94fe57
Archangel354 <rduncan354@gmail.com>
2019-05-15 15:06:17 -0400

    Updated DetailAdapter with standard methods

--- app/src/main/java/com/example/baking/DetailAdapter.java
+++ app/src/main/java/com/example/baking/DetailAdapter.java
@@ -1,4 +1,32 @@
 package com.example.baking;
 
-public class DetailAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class DetailAdapter extends RecyclerView.Adapter<DetailAdapter> {
+    private Context dContext;
+    private ArrayList<ExampleItem> mExampleList;
+    private OnItemClickListener mListener;
+
+
+
+    @NonNull
+    @Override
+    public DetailAdapter onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DetailAdapter detailAdapter, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
