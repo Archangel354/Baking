4b01f8c217bac34fb212bdd9f3cac70aaa714766
Archangel354 <rduncan354@gmail.com>
2019-05-21 13:58:03 -0400

    Updated DetailAdapter by creating a constructor and implementing the 3 methods

--- app/src/main/java/com/example/baking/DetailAdapter.java
+++ app/src/main/java/com/example/baking/DetailAdapter.java
@@ -3,6 +3,7 @@ package com.example.baking;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -27,18 +28,24 @@ public class DetailAdapter extends RecyclerView.Adapter<DetailAdapter.DetailView
 
     @NonNull
     @Override
-    public DetailViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public DetailViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View v = LayoutInflater.from(dContext).inflate(R.layout.ingredients_steps_item, parent, false);
+        return new DetailViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull DetailViewHolder detailViewHolder, int i) {
+    public void onBindViewHolder(@NonNull DetailViewHolder detailViewHolder, int position) {
+        BakingModel currentItem = mExampleList.get(position);
+
+        String ingredients = currentItem.getIngredientsList();
+
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mExampleList.size();
     }
 
     public class DetailViewHolder extends RecyclerView.ViewHolder{
