f2b7fe3afe5f35eed4670d827d8edda83d0a89ec
Archangel354 <rduncan354@gmail.com>
2019-05-20 21:49:33 -0400

    added a constructor to the DetailAdapter.java file

--- app/src/main/java/com/example/baking/DetailAdapter.java
+++ app/src/main/java/com/example/baking/DetailAdapter.java
@@ -1,16 +1,30 @@
-package com.example.baking;
+spackage com.example.baking;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.baking.models.BakingModel;
 
 import java.util.ArrayList;
 
-public class DetailAdapter extends RecyclerView.Adapter<DetailAdapter> {
+public class DetailAdapter extends RecyclerView.Adapter<DetailAdapter.DetailViewHolder> {
     private Context dContext;
-    private ArrayList<ExampleItem> mExampleList;
-    private OnItemClickListener mListener;
+    //private ArrayList<ExampleItem> mExampleList;
+   // private OnItemClickListener mListener;
+
+    public class DetailViewHolder extends RecyclerView.ViewHolder{
+        public TextView dTextIngredients;
+        public ArrayList<BakingModel.Steps> dStepsList;
+
+        public DetailViewHolder(@NonNull View itemView) {
+            super(itemView);
+            dTextIngredients =
+        }
+    }
 
 
 
