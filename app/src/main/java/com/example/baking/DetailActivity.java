44eee7a8416e808bbfc195a29d50be4f286107ee
Archangel354 <rduncan354@gmail.com>
2019-05-25 20:56:40 -0400

    Added the header "List of Ingredients" activity_detail.xml.  Removed the word "null" from the list of ingredients string

--- app/src/main/java/com/example/baking/DetailActivity.java
+++ app/src/main/java/com/example/baking/DetailActivity.java
@@ -3,8 +3,14 @@ package com.example.baking;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
+
+import com.example.baking.models.BakingModel;
+
+import java.util.ArrayList;
 
 import static com.example.baking.MainActivity.EXTRA_INGREDIENTS;
 
@@ -12,18 +18,30 @@ import static com.example.baking.MainActivity.EXTRA_INGREDIENTS;
 public class DetailActivity extends AppCompatActivity {
 
     private RecyclerView dRecyclerView;
+    private ArrayList<BakingModel.Steps> dStepsList;
+
+    /** Adapter for the gridview of movies from the JSON data */
+    private DetailAdapter dAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        dRecyclerView =  findViewById(R.id.recyclerDetail_view);
+        // Create a new adapter that takes an empty list of movies as input
+        dStepsList =
+        dAdapter = new DetailAdapter(DetailActivity.this, dStepsList);
+        dRecyclerView.setHasFixedSize(true);
+        dRecyclerView.setLayoutManager(new LinearLayoutManager(DetailActivity.this));
+
+        // Set the adapter on the {@link GridView} so the list can be populated in the user interface
+        dRecyclerView.setAdapter(dAdapter);
 
         Intent intent = getIntent();
         String ingredients = intent.getStringExtra(EXTRA_INGREDIENTS);
 
         TextView textIngredients = findViewById(R.id.txtIngredientsList);
-        //dRecyclerView = findViewById(R.id.recyclerDetail_view);
         textIngredients.setText(ingredients);
 
     }
--- app/src/main/java/com/example/baking/DetailAdapter.java
+++ app/src/main/java/com/example/baking/DetailAdapter.java
@@ -8,24 +8,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.baking.R;
 import com.example.baking.models.BakingModel;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class DetailAdapter extends RecyclerView.Adapter<DetailAdapter.DetailViewHolder> {
     private Context dContext;
-    private ArrayList<BakingModel> mExampleList;
-   // private OnItemClickListener mListener;
+    private ArrayList<BakingModel.Steps> mStepList;
+    //private RecyclerView dRecyclerView;
+    // private OnItemClickListener mListener;
 
 
-    public DetailAdapter(Context context, ArrayList<BakingModel> exampleList) {
+    public DetailAdapter(Context context, ArrayList<BakingModel.Steps> stepList) {
         dContext = context;
-        mExampleList = exampleList;
+        mStepList = stepList;
     }
 
     @NonNull
@@ -37,32 +34,28 @@ public class DetailAdapter extends RecyclerView.Adapter<DetailAdapter.DetailView
 
     @Override
     public void onBindViewHolder(@NonNull DetailViewHolder detailViewHolder, int position) {
-        BakingModel currentItem = mExampleList.get(position);
+        BakingModel.Steps currentItem = mStepList.get(position);
 
-        String ingredients = currentItem.getIngredientsList();
-        List steps = currentItem.getStepsList();
+        String ingredients = currentItem.getShortDescription();
+        //List steps = currentItem.getStepsList();
 
-        detailViewHolder.dTextIngredients.setText(ingredients);
+        detailViewHolder.dTextStep.setText(ingredients);
         //detailViewHolder.dTextStep.List(steps);
-
-
-
-
     }
 
     @Override
     public int getItemCount() {
-        return mExampleList.size();
+        return mStepList.size();
     }
 
     public class DetailViewHolder extends RecyclerView.ViewHolder{
-        public TextView dTextIngredients;
+        //public TextView dTextIngredients;
         public TextView dTextStep;
 
         public DetailViewHolder(@NonNull View itemView) {
             super(itemView);
-            dTextIngredients = itemView.findViewById(R.id.txtIngredients);
-            //dTextStep = itemView.findViewById(R.id.rvSteps);
+            //dTextIngredients = itemView.findViewById(R.id.txtIngredients);
+            dTextStep = itemView.findViewById(R.id.txtStep);
         }
     }
 }
