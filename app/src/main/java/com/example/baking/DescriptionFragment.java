8b507cb0e13d3d49e5a24d17cc6d1dd7666e6a33
Archangel354 <rduncan354@gmail.com>
2019-07-07 15:38:45 -0400

    Updated DescriptionFragment from its original template form

--- app/src/main/java/com/example/baking/DescriptionFragment.java
+++ app/src/main/java/com/example/baking/DescriptionFragment.java
@@ -8,6 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,14 +21,14 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class DescriptionFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
+    // DONE: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public static final String DESCRIPTION_ID_LIST = "description_ids";
+    public static final String LIST_INDEX = "list_index";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // DONE: Rename and change types of parameters
+    private ArrayList<Integer> mDescriptionIds;
+    private int mListIndex;
 
     private OnFragmentInteractionListener mListener;
 
@@ -37,28 +40,28 @@ public class DescriptionFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param mDescriptionIds Parameter 1.
+     * @param mListIndex Parameter 2.
      * @return A new instance of fragment DescriptionFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static DescriptionFragment newInstance(String param1, String param2) {
+    // DONE: Rename and change types and number of parameters
+    public static DescriptionFragment newInstance(ArrayList<Integer> mDescriptionIds, int mListIndex) {
         DescriptionFragment fragment = new DescriptionFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putIntegerArrayList(DESCRIPTION_ID_LIST, mDescriptionIds);
+        args.putInt(LIST_INDEX, mListIndex);
         fragment.setArguments(args);
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mDescriptionIds = getArguments().getString(DESCRIPTION_ID_LIST);
+//            mListIndex = getArguments().getString(LIST_INDEX);
+//        }
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
