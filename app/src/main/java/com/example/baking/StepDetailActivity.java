965f12e83287dd2110092f4462a5186ad5e6cee4
Archangel354 <rduncan354@gmail.com>
2019-07-23 14:43:14 -0400

    I can increment to the 2nd step but that is where it stays.

--- app/src/main/java/com/example/baking/StepDetailActivity.java
+++ app/src/main/java/com/example/baking/StepDetailActivity.java
@@ -24,6 +24,8 @@ public class StepDetailActivity extends AppCompatActivity implements View.OnClic
     private Button btnNext;
     private BakingModel.Steps steps;
     private ArrayList<BakingModel.Steps> dStepsList;
+    private int mListIndex;
+    private int stepListsize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +36,14 @@ public class StepDetailActivity extends AppCompatActivity implements View.OnClic
         btnPrevious = findViewById(R.id.btnPrevious);
         btnNext = findViewById(R.id.btnNext);
 
+
         final Intent stepDetailIntent = getIntent();
         final int position = stepDetailIntent.getIntExtra(EXTRA_POSITION, 0);
+        mListIndex = position;
         final ArrayList<BakingModel.Steps> stepList = stepDetailIntent.getParcelableArrayListExtra(EXTRA_STEPLIST);
         final String sDescription = stepList.get(position).getDescription();
         final String sUrlString = stepList.get(position).getVideoURL();
-        Integer arraySize = stepList.size();
-        String mDescription = stepList.get(position + 1).getDescription();
+        stepListsize = stepList.size();
 
         Log.i("StepDetailActivity", "position: " + position + ".");
         Log.i("StepDetailActivity", "description: " + sDescription + ".");
@@ -49,11 +52,32 @@ public class StepDetailActivity extends AppCompatActivity implements View.OnClic
         btnPrevious.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int position = stepDetailIntent.getIntExtra(EXTRA_POSITION, 0);
-                Toast.makeText(StepDetailActivity.this, "Previous position is: " + position, Toast.LENGTH_SHORT).show();
-                if (position <= 0) {
-                    Toast.makeText(StepDetailActivity.this, "No previous position available " + position, Toast.LENGTH_SHORT).show();
-                    return;
+                if (mListIndex > 0){
+
+                    mListIndex--;
+                    Log.i("Previous", "mListIndex " + mListIndex);
+                    Log.i("Previous", "stepListsize " + stepListsize);
+
+                    //Intent stepDetailIntent = new Intent(v.getContext(), StepDetailActivity.class);
+                    BakingModel.Steps clickedItem = stepList.get(mListIndex);
+
+                    Log.i("Previous", "step " + clickedItem.getDescription());
+                    Log.i("Previous", "video " + clickedItem.getVideoURL());
+                    Log.i("Previous", "steps " + stepList);
+                    //Bundle arguments = new Bundle();
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    Bundle arguments = new Bundle();
+                    arguments.putString(EXTRA_STEP, clickedItem.getDescription());
+                    arguments.putString(EXTRA_VIDEO,clickedItem.getVideoURL());
+                    StepFragment stepFragment = new StepFragment();
+                    stepFragment.setArguments(arguments);
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.step_container, stepFragment)
+                            .commit();
+
+                }else{
+                    Toast.makeText(StepDetailActivity.this, "No more steps before this!!", Toast.LENGTH_SHORT).show();
+
                 }
             }
         });
@@ -64,12 +88,15 @@ public class StepDetailActivity extends AppCompatActivity implements View.OnClic
                 //Toast.makeText(StepDetailActivity.this, "Next Pressed", Toast.LENGTH_SHORT).show();
                /// Toast.makeText(StepDetailActivity.this, "Next position is: " + position, Toast.LENGTH_SHORT).show();
                // Toast.makeText(StepDetailActivity.this, "Next dStepsList is: " + dStepsList, Toast.LENGTH_SHORT).show();
-                Integer nextPosition = position + 1;
-                Log.i("Next", "nextPosition: " + nextPosition);
-                Log.i("Next", "stepList " + stepList);
+                if (mListIndex < stepListsize -1){
 
-                Intent stepDetailIntent = new Intent(v.getContext(), StepDetailActivity.class);
-                BakingModel.Steps clickedItem = stepList.get(position + 1);
+                mListIndex++;
+                Log.i("Next", "mListIndex " + mListIndex);
+                Log.i("Next", "stepListsize " + stepListsize);
+
+                //Intent stepDetailIntent = new Intent(v.getContext(), StepDetailActivity.class);
+                BakingModel.Steps clickedItem = stepList.get(mListIndex);
+
                 Log.i("Next", "step " + clickedItem.getDescription());
                 Log.i("Next", "video " + clickedItem.getVideoURL());
                 Log.i("Next", "steps " + stepList);
@@ -84,15 +111,15 @@ public class StepDetailActivity extends AppCompatActivity implements View.OnClic
                         .replace(R.id.step_container, stepFragment)
                         .commit();
 
+            }else{
+                        Toast.makeText(StepDetailActivity.this, "No more steps after this!!", Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
 
         if (savedInstanceState == null) {
-            // In two-pane mode, add initial BodyPartFragments to the screen
             FragmentManager fragmentManager = getSupportFragmentManager();
-            Log.i("StepDetailActivity", "mDescription: " + mDescription + ".");
-
-            //String step = getIntent().getStringExtra(EXTRA_STEP);
             Bundle arguments = new Bundle();
             arguments.putString(EXTRA_STEP, sDescription);
             arguments.putString(EXTRA_VIDEO, sUrlString);
