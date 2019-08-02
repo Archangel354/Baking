6c19055790226300c432a0dfca0f3882a4e6c36b
Archangel354 <rduncan354@gmail.com>
2019-08-01 22:03:42 -0400

    Added intent and pending intent to RecipeWidget.java. added src" @drawable/cooking to recipe_widget.xml

--- app/src/main/java/com/example/baking/RecipeWidget.java
+++ app/src/main/java/com/example/baking/RecipeWidget.java
@@ -1,8 +1,10 @@
 package com.example.baking;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 /**
@@ -13,10 +15,15 @@ public class RecipeWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.widget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        // Create an Intent to launch MainaActivity when widget is clicked
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        //Widgets allow click handlers to only launch pending intents
+        views.setOnClickPendingIntent(R.id.imgChef, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
